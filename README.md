02f561f403f08796bfb948e20c463edae96aac37
Youssof Yassin <youssofwyassin@gmail.com>
2021-12-06 14:53:04 +0200

    Added constraints and explanations

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
+### Project description
+
 The purpose of this database is to provide the startup, Sparkify, with a way to record its data in an organised manner. 
-A Star Schema was used here to simplify and speed up aggregations, so that the user does not have to wait a long time to find answers to their analytical questions.
-Our schema consists of songplays as the fact table, as well as songs, artists, time and user as the dimension tables that connect to it.
+We have a lot of song and log data from multiple files in json format that we need to set up into tables.
+Currently these files on their own are very difficult to read and understand. To solve these problems, this project creates a database and
+records everything, allowing anyone to read, analyse and update more info if needed.
+
+### Database Design
+
+![ERD](sparkifydb_erd.png)
+
+A Star Schema was used here to simplify and speed up aggregations, so that the user does not have to wait a 
+long time to find answers to their analytical questions. In detail:
+
+- songplays: Fact Table that contains the main information
+- songs: Dimension table that connects to songplays through song_id; contains only song related data.
+- artists: Dimension table that connects to songplays through artist_id; contains artist specific data.
+- time: Dimension table that connects to songplays through start_time; records and time and date info.
+- users: Dimension table that connects to songplays through user_id; contains user specific data.
+
+### ETL Process
+
+The data is all contained in the aptly named data folder. 
+2 types of files are used to record this informationinto our database: songs and logs.
+From song files, we record song and artist data, whereas from log files, we record time data and user data.
+Finally, we query the song name, artist name and song duration to help us fill in our songplays fact table.
+
+### Project Repository files
 
 2 types of files are used to record this information into our database: songs and logs. 
 From song files, we record song and artist data, whereas from log files, we record time data and user data.
-Finally, we query the song name, artist name and song duration to help us fill in our songplays fact table.
+Finally, we query the song name, artist name and song duration to help us fill in our songplays fact table
+
+### How To Run the Project
+
+Simply run create_tables.py and then run test.ipynb to make sure no errors pop up and that it! The database has been created
+
--- etl.py
+++ etl.py
@@ -6,19 +6,45 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    '''Opens a given filepath to a song and obtains the song and artist info.
+    Then, all of it is inserted into the song and artist tables.
+    
+    Args:
+         cur: cursor that points to the the connection to the server
+         filepath: the location of the songfile to be processed
+    '''
     # open song file
     df = pd.read_json(filepath, typ='series')
 
     # insert song record
-    song_data = df[['song_id','title','artist_id','year','duration']].values.tolist()
+    song_data = df[[
+        'song_id',
+        'title',
+        'artist_id',
+        'year',
+        'duration',
+    ]].values.tolist()
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = df[['artist_id','artist_name','artist_location','artist_latitude','artist_longitude']].replace({pd.np.nan:None}).values.tolist()
+    artist_data = df[[
+        'artist_id',
+        'artist_name',
+        'artist_location',
+        'artist_latitude',
+        'artist_longitude',
+    ]].replace({pd.np.nan:None}).values.tolist()
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
+    '''looks through a a given filepath to a logfile that contains information
+    in order to populate the time, users and songplay tables.
+    
+    Args:
+        cur: cursor that points to the the connection to the server
+        filepath: location of the logfile
+    '''
     # open log file
     df = pd.read_json(filepath, lines=True)
 
@@ -29,15 +55,38 @@ def process_log_file(cur, filepath):
     t = pd.to_datetime(df.ts, unit = 'ms')
     
     # insert time data records
-    time_data = [t.dt.strftime('%H:%M:%S'), t.dt.hour, t.dt.day, t.dt.week, t.dt.month, t.dt.year, t.dt.weekday]
-    column_labels = ['start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday']
+    time_data = [
+        t.dt.strftime('%H:%M:%S'), 
+        t.dt.hour, 
+        t.dt.day, 
+        t.dt.week, 
+        t.dt.month, 
+        t.dt.year, 
+        t.dt.weekday,
+    ]
+    
+    column_labels = [
+        'start_time', 
+        'hour', 
+        'day', 
+        'week', 
+        'month', 
+        'year', 
+        'weekday',
+    ]
     time_df = pd.DataFrame(time_data, index=column_labels).T
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = df[['userId','firstName','lastName','gender','level']]
+    user_df = df[[
+        'userId',
+        'firstName',
+        'lastName',
+        'gender',
+        'level',
+    ]]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -56,11 +105,29 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = (time_df.loc[index, 'start_time'], row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+        songplay_data = (
+            time_df.loc[index, 'start_time'], 
+            row.userId, 
+            row.level, 
+            songid, 
+            artistid, 
+            row.sessionId, 
+            row.location, 
+            row.userAgent,
+        )
         cur.execute(songplay_table_insert, songplay_data)
 
 
-def process_data(cur, conn, filepath, func):
+def process_data(cur, conn, filepath, func):'''
+    loop through all the directories in the provided file path and process
+    each .json file found.
+    
+    Args:
+        cur: cursor that points to the the connection to the server
+        conn: the connection to the server; used to commit
+        filepath: the file path to look through
+        func: function to use to process the files
+    '''
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -80,6 +147,8 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
+    '''connect to our host with the credentials and process the files into the DB
+    '''
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
